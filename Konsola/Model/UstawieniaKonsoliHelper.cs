bc3f82bae11a1e90bf6fbc1d5fbd2d0f96132b3a
Butcher <phil.nevermind@gmail.com>
2023-04-30 21:06:30 +0200

    Dodanie kodu projektu

---
+++ Konsola/Model/UstawieniaKonsoliHelper.cs
@@ -0,0 +1,38 @@
+﻿namespace Konsola.Model
+{
+    public static class UstawieniaKonsoliHelper
+    {
+        public static UstawieniaKonsoli UstawieniaBiezace
+        {
+            get
+            {
+                return new UstawieniaKonsoli()
+                {
+                    KolorTla = Console.BackgroundColor,
+                    KolorCzcionki = Console.ForegroundColor,
+                    RozmiarOkna =
+                    {
+                        Szerokosc = Console.WindowWidth,
+                        Wysokosc = Console.WindowHeight
+                    },
+                    RozmiarBufora =
+                    {
+                        Szerokosc = Console.BufferWidth,
+                        Wysokosc = Console.BufferHeight
+                    },
+                    Tytul = Console.Title
+                };
+            }
+        }
+
+        public static UstawieniaKonsoli UstawieniaDomyslne { get; } =
+            new UstawieniaKonsoli()
+            {
+                KolorTla = ConsoleColor.Black,
+                KolorCzcionki = ConsoleColor.Gray,
+                RozmiarOkna = { Szerokosc = 120, Wysokosc = 30 },
+                RozmiarBufora = { Szerokosc = 120, Wysokosc = 9001 },
+                Tytul = System.Reflection.Assembly.GetEntryAssembly().Location
+            };
+    }
+}
